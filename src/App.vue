d6ccbfb83c57aad730f6e444cf60e9126ec66bb7
Natalie Mastracci <mastracci.natalie@gmail.com>
2020-03-21 15:37:59 -0400

    sets up initial structure

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1 id="heading">Commented.</h1>
+    <div class="container">
+      <Comment msg="Welcome to Your Vue.js App" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Comment from './components/Comment.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Comment,
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #e5f0fa;
+}
+h4 {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#heading {
+  font-size: 3rem;
+  text-transform: lowercase;
+  font-family: 'Roboto Slab', serif;
+  color: #212529;
+  // color: #4e4d94;
+}
+.container {
+  background-color: #fff;
+  box-shadow: 0px 4px 8px 0px rgba(73, 80, 87, 0.25);
+  // box-shadow: 0px 4px 8px 0px rgb(199, 175, 163);
+
+  width: 40rem;
+  margin: 0 auto;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+}
+</style>
---
+++ src/components/Comment.vue
@@ -0,0 +1,117 @@
+<template>
+  <div
+    @mouseover="isHovering = false"
+    @mouseout="isHovering = true"
+    class="comment"
+  >
+    <div class="user">
+      <div class="circle"></div>
+    </div>
+    <div>
+      <div class="card-title">
+        <h4 class="">Brad</h4>
+        <h4 class=" tag">Author</h4>
+        <i>&#183;</i>
+        <h4 :class="[{ 'text-light': isHovering }, 'secondary-text']">
+          2 minutes ago
+        </h4>
+      </div>
+      <p>
+        So what the German automaker is likey to focus on today is the bigger
+        picture. Thishi will be the first time we see the Taycan free from any
+        prototype bodywork.
+      </p>
+      <div class="interactions">
+        <span :class="[{ 'text-light': isHovering }, 'mr', 'secondary-text']"
+          >Reply</span
+        >
+        <span :class="[{ 'text-light': isHovering }, 'mr', 'secondary-text']"
+          >21 Replies</span
+        >
+        <span :class="[{ 'text-light': isHovering }, 'mr', 'secondary-text']"
+          >^ 123</span
+        >
+        <span :class="[{ 'text-light': isHovering }, 'mr', 'secondary-text']"
+          >^ 0</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  data: () => {
+    return {
+      isHovering: true,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.comment {
+  border-top: 0.0625rem solid #e9ecef;
+  border-bottom: 0.0625rem solid #e9ecef;
+  display: grid;
+  grid-template-columns: 1fr 7fr;
+  grid-template-rows: 1fr;
+  text-align: left;
+  padding: 0.75rem;
+  &:hover {
+    color: #212529;
+  }
+}
+.user {
+  min-width: 3rem;
+}
+.interactions {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  &:hover {
+    color: #212529;
+  }
+}
+.secondary-text {
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  &:hover {
+    color: #212529;
+  }
+}
+.text-light {
+  color: #e9ecef;
+}
+.mr {
+  margin-right: 1.25rem;
+}
+.tag {
+  background-color: #212529;
+  border-radius: 2rem;
+  padding: 0.125rem 0.5rem;
+  color: #fff;
+  font-size: 0.75rem;
+  text-transform: uppercase;
+}
+.circle {
+  width: 3rem;
+  height: 3rem;
+  background: #ff7f00;
+  // #d9a967
+  border-radius: 50%;
+}
+.card-title {
+  @extend .interactions;
+  h4 {
+    margin-right: 0.5rem;
+  }
+  i {
+    margin-right: 0.5rem;
+    font-size: 1.5rem;
+    color: #e9ecef;
+  }
+}
+</style>
