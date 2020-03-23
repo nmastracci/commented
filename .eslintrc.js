0fdbd2bfd5352d4e590ddffeccdd6c7fda5a9ec9
Natalie Mastracci <mastracci.natalie@gmail.com>
2020-03-22 20:57:47 -0400

    adds single quote rule to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'never'],
-    quotes: [2, 'single', { avoidEscape: true }]
+    "prettier/prettier": ["error", {"singleQuote": true, "parser": "flow"}],
+    quotes: ['error', 'single', { avoidEscape: true, "allowTemplateLiterals": true }]
   }
 };
